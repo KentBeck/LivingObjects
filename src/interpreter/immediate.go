13dee5f2949cee9e598532eb3874ba54b03f46da
Kent Beck <kentlbeck@gmail.com>
2025-04-23 06:00:04 -0700

    merge immediates

--- src/interpreter/immediate.go
+++ src/interpreter/immediate.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"unsafe"
 )
 
@@ -121,4 +122,37 @@ func GetIntegerImmediate(obj *Object) int64 {
 	}
 
 	return int64(unsigned)
+}
+
+// IsFloatImmediate returns true if the value is an immediate float
+func IsFloatImmediate(obj *Object) bool {
+	// Convert the pointer to an integer
+	ptr := uintptr(unsafe.Pointer(obj))
+
+	// Check if the tag is TAG_FLOAT
+	return (ptr & TAG_MASK) == TAG_FLOAT
+}
+
+// MakeFloatImmediate returns an immediate float value
+func MakeFloatImmediate(value float64) *Object {
+	// Convert the float to bits
+	bits := math.Float64bits(value)
+	// We lose some precision, but that's acceptable for most use cases
+	// The bottom 2 bits are used for the tag
+	imm := (bits >> 2 << 2) | TAG_FLOAT
+
+	// Convert to a pointer
+	return (*Object)(unsafe.Pointer(uintptr(imm)))
+}
+
+// GetFloatImmediate extracts the float value from an immediate float
+func GetFloatImmediate(obj *Object) float64 {
+	// Convert the pointer to an integer
+	ptr := uintptr(unsafe.Pointer(obj))
+
+	// Remove the tag bits
+	bits := ptr & ^uintptr(TAG_MASK)
+
+	// Convert to float64
+	return math.Float64frombits(uint64(bits))
 }
