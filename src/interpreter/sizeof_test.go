0398d4093dc54eb232dd2da400042faa26a3ed0e
Kent Beck <kentlbeck@gmail.com>
2025-05-06 18:18:50 -0700

    Restructure interpreter into modular packages

--- src/interpreter/sizeof_test.go
+++ src/interpreter/sizeof_test.go
@@ -11,6 +11,7 @@ func TestSizeOfObject(t *testing.T) {
 
 	// Assert that the size is less than or equal to 232 bytes
 	const maxSize = 232
+	t.Log("Object size is", size, "bytes")
 	if size > maxSize {
 		t.Errorf("Object size is %d bytes, which exceeds the maximum allowed size of %d bytes", size, maxSize)
 	}
