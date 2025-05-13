6531db0fd737cd610470ebd708de1c2dd515b909
Kent Beck <kentlbeck@gmail.com>
2025-05-13 16:35:38 -0700

    better

--- src/interpreter/vm/block_factory.go
+++ src/interpreter/vm/block_factory.go
@@ -7,7 +7,15 @@ import (
 
 // NewBlock creates a block object with proper class field
 func (vm *VM) NewBlock(outerContext interface{}) *core.Object {
-	block := classes.NewBlockInternal(outerContext)
+	block := &classes.Block{
+		Object: core.Object{
+			TypeField: core.OBJ_BLOCK,
+		},
+		Bytecodes:    make([]byte, 0),
+		Literals:     make([]*core.Object, 0),
+		TempVarNames: make([]string, 0),
+		OuterContext: outerContext,
+	}
 	blockObj := classes.BlockToObject(block)
 	blockObj.SetClass(classes.ClassToObject(vm.Classes.Get(Block)))
 	return blockObj
