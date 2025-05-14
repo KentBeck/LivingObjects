5b9964fbefc3f5224e732abecbcbdd31c8f7bd02
Kent Beck <kentlbeck@gmail.com>
2025-05-13 18:45:06 -0700

    volunteered bytearray

--- src/interpreter/classes/byte_array.go
+++ src/interpreter/classes/byte_array.go
@@ -13,8 +13,6 @@ type ByteArray struct {
 	Bytes []byte
 }
 
-// ByteArray is created by the VM using NewByteArray
-
 // ByteArrayToObject converts a ByteArray to an Object
 func ByteArrayToObject(ba *ByteArray) *core.Object {
 	return (*core.Object)(unsafe.Pointer(ba))
@@ -49,60 +47,4 @@ func (ba *ByteArray) AtPut(index int, value byte) {
 		panic("index out of bounds")
 	}
 	ba.Bytes[index] = value
-}
-
-// Copy returns a copy of the byte array
-func (ba *ByteArray) Copy() *ByteArray {
-	newBA := &ByteArray{
-		Object: core.Object{
-			TypeField: core.OBJ_BYTE_ARRAY,
-		},
-		Bytes: make([]byte, len(ba.Bytes)),
-	}
-	copy(newBA.Bytes, ba.Bytes)
-	return newBA
-}
-
-// CopyFrom returns a new byte array containing the bytes from startIndex to endIndex
-func (ba *ByteArray) CopyFrom(startIndex, endIndex int) *ByteArray {
-	if startIndex < 0 || startIndex >= len(ba.Bytes) {
-		panic("start index out of bounds")
-	}
-	if endIndex < startIndex || endIndex >= len(ba.Bytes) {
-		panic("end index out of bounds")
-	}
-
-	newSize := endIndex - startIndex + 1
-	newBA := &ByteArray{
-		Object: core.Object{
-			TypeField: core.OBJ_BYTE_ARRAY,
-		},
-		Bytes: make([]byte, newSize),
-	}
-	copy(newBA.Bytes, ba.Bytes[startIndex:endIndex+1])
-	return newBA
-}
-
-// Uint32At reads a 32-bit unsigned integer from the byte array at the given index
-func (ba *ByteArray) Uint32At(index int) uint32 {
-	if index < 0 || index+3 >= len(ba.Bytes) {
-		panic("index out of bounds for uint32")
-	}
-
-	return uint32(ba.Bytes[index]) |
-		uint32(ba.Bytes[index+1])<<8 |
-		uint32(ba.Bytes[index+2])<<16 |
-		uint32(ba.Bytes[index+3])<<24
-}
-
-// Uint32AtPut writes a 32-bit unsigned integer to the byte array at the given index
-func (ba *ByteArray) Uint32AtPut(index int, value uint32) {
-	if index < 0 || index+3 >= len(ba.Bytes) {
-		panic("index out of bounds for uint32")
-	}
-
-	ba.Bytes[index] = byte(value)
-	ba.Bytes[index+1] = byte(value >> 8)
-	ba.Bytes[index+2] = byte(value >> 16)
-	ba.Bytes[index+3] = byte(value >> 24)
 }
